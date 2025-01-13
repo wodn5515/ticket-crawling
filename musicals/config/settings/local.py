b701d8eeb50c99768ecf5c032efa2ea820d56fed
Jaewoo Kil <wodn5515@gmail.com>
2025-01-13 13:14:35 +0900

    init 및 기본세팅

---
+++ musicals/config/settings/local.py
@@ -0,0 +1,13 @@
+from config.settings.base import *
+import os
+
+DATABASES = {
+    "default": {
+        "ENGINE": "django.db.backends.postgresql",
+        "NAME": os.environ.get("DB_NAME"),
+        "USER": os.environ.get("DB_USER"),
+        "PASSWORD": os.environ.get("DB_PASSWORD"),
+        "HOST": os.environ.get("DB_HOST"),
+        "PORT": os.environ.get("DB_PORT"),
+    }
+}
