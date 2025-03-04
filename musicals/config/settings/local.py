bb986ab43d2c4846d021f8dc6a98cd564e37ae56
Jaewoo Kil <wodn5515@gmail.com>
2025-03-04 18:36:58 +0900

    env 네이밍 수정

--- musicals/config/settings/local.py
+++ musicals/config/settings/local.py
@@ -3,15 +3,13 @@ import os
 
 DEBUG = True
 
-INSTALLED_APPS += ["django_extensions"]
-
 DATABASES = {
     "default": {
         "ENGINE": "django.db.backends.postgresql",
-        "NAME": os.environ.get("DB_NAME"),
-        "USER": os.environ.get("DB_USER"),
-        "PASSWORD": os.environ.get("DB_PASSWORD"),
-        "HOST": os.environ.get("DB_HOST"),
-        "PORT": os.environ.get("DB_PORT"),
+        "HOST": os.environ.get("POSTGRES_HOST"),
+        "NAME": os.environ.get("POSTGRES_NAME"),
+        "USER": os.environ.get("POSTGRES_USER"),
+        "PASSWORD": os.environ.get("POSTGRES_PASSWORD"),
+        "PORT": os.environ.get("POSTGRES_PORT"),
     }
 }
